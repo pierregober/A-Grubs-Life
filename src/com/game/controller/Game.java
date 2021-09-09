b3c2c60723393d1b3f9ebb6a442db929d64443c3
CleverlyDone <pierregober@gmail.com>
2021-09-09 13:27:55 -0700

    Made html files for the html strings, AB#416

--- src/com/game/controller/Game.java
+++ src/com/game/controller/Game.java
@@ -30,9 +30,9 @@ public class Game {
     }
 
     /**
-     *   Called by the client to start a new game.
+     * Called by the client to start a new game.
      */
-    public void start(){
+    public void start() {
         setUpComponents();
         run();
     }
@@ -40,21 +40,20 @@ public class Game {
     /**
      * Instantiates the necessary fields of a Game object.
      */
-    private void setUpComponents(){
+    private void setUpComponents() {
         this.enemies = populateEnemies();
         this.locations = populateLocations();
-        this.caterpillar = new Caterpillar(100,0,0);
-        this.processor = new LogicEngine(caterpillar,locations, enemies);
+        this.caterpillar = new Caterpillar(100, 0, 0);
+        this.processor = new LogicEngine(caterpillar, locations, enemies);
         this.caterpillar.setCurrentLocation(locations.get("GENESIS"));
         this.viewWindow = new ViewWindow(caterpillar, processor);
     }
 
     /**
-     *  Controls the game loop.
-     *  View updates with user input.
-     *
+     * Controls the game loop.
+     * View updates with user input.
      */
-    private void run(){
+    private void run() {
         int counter = 0;
         viewWindow.welcomeMessage();
         while (true) {
@@ -66,10 +65,11 @@ public class Game {
 
     /**
      * Generates location objects from text file data
+     *
      * @return hashmap of location objects (String, Location)
      */
-    private HashMap<String,Location> populateLocations(){
-        HashMap<String,Location> locations = new HashMap<>();
+    private HashMap<String, Location> populateLocations() {
+        HashMap<String, Location> locations = new HashMap<>();
         String[] locationFields;
         //                locationFields = myReader.nextLine().split(",");
 //
@@ -79,15 +79,15 @@ public class Game {
 //        File file = new File(getClass().getResource("src/com/game/locations.txt").toURI());
 //            Scanner myReader = new Scanner(file);
 //            while(myReader.hasNextLine()){
-        try{
+        try {
             InputStream inputStream = getClass().getResourceAsStream("locations.txt");
             InputStreamReader myReader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(myReader);
             String line = null;
-            while((line = br.readLine() )!= null){
+            while ((line = br.readLine()) != null) {
                 locationFields = line.split(",");
 
-                Location loc = new Location(locationFields[0].trim(),locationFields[ 1].trim(), locationFields[ 2].trim(), locationFields[ 3].trim(), locationFields[4].trim(),locationFields[ 5].trim() );
+                Location loc = new Location(locationFields[0].trim(), locationFields[1].trim(), locationFields[2].trim(), locationFields[3].trim(), locationFields[4].trim(), locationFields[5].trim());
                 loc.setEnemy(enemies.get(locationFields[0].trim().toLowerCase(Locale.ROOT)));
                 locations.put(locationFields[0].trim(), loc);
             }
@@ -103,9 +103,10 @@ public class Game {
 
     /**
      * Populates Enemy objects from an external text file.
+     *
      * @return Hashmap of Enemy objects (String, Enemy)
      */
-    private HashMap<String,Enemy> populateEnemies() {
+    private HashMap<String, Enemy> populateEnemies() {
         HashMap<String, Enemy> enemies = new HashMap<>();
 
         String[] enemyFields;
@@ -114,10 +115,9 @@ public class Game {
             InputStreamReader myReader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(myReader);
             String line = null;
-            while((line = br.readLine() )!= null){
+            while ((line = br.readLine()) != null) {
                 enemyFields = line.split(",");
-
-                Enemy enemy = new Enemy(enemyFields[0].trim(),Integer.parseInt(enemyFields[ 1].trim()), Integer.parseInt(enemyFields[ 2].trim()), Integer.parseInt(enemyFields[ 3].trim()), Boolean.parseBoolean(enemyFields[4].trim()), Boolean.parseBoolean(enemyFields[5].trim()), enemyFields[6].trim(), Boolean.parseBoolean(enemyFields[7].trim()));
+                Enemy enemy = new Enemy(enemyFields[0].trim(), Integer.parseInt(enemyFields[1].trim()), Integer.parseInt(enemyFields[2].trim()), Integer.parseInt(enemyFields[3].trim()), Boolean.parseBoolean(enemyFields[4].trim()), Boolean.parseBoolean(enemyFields[5].trim()), enemyFields[6].trim(), Boolean.parseBoolean(enemyFields[7].trim()));
                 enemies.put(enemyFields[6].trim(), enemy);
 
             }
@@ -133,6 +133,7 @@ public class Game {
 
     /**
      * Get a hashmap of enemies in the game
+     *
      * @return Hashmap of Enemy objects (String, Enemy)
      */
     public HashMap<String, Enemy> getEnemies() {
