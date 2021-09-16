ebdacf262799caf58b21a8098608a701de758ab0
CleverlyDone <pierregober@gmail.com>
2021-09-16 08:19:30 -0700

    Add back the caterpillar status method call

--- src/com/game/view/ViewWindow.java
+++ src/com/game/view/ViewWindow.java
@@ -256,6 +256,7 @@ public class ViewWindow {
             updateDescriptionPanel();
             updateMapPanel();
             updateLastMove();
+            updateCaterpillarStatus();
         });
     }
 
