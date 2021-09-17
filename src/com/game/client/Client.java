669be94fb502b48393e864aaf935e8008d78578a
CleverlyDone <pierregober@gmail.com>
2021-09-17 08:51:39 -0700

    fixed the wiki link and added a test, AB#553

--- src/com/game/client/Client.java
+++ src/com/game/client/Client.java
@@ -1,9 +1,10 @@
 package com.game.client;
 
 import com.game.controller.Game;
+import java.net.URISyntaxException;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         Game game = new Game();
         game.start();
     }
